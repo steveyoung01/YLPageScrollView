b5098b9d8b3a5aa34904229f9dc49b99014dcb6c
yl@bdtrip.com.cn <920398536@qq.com>
2017-04-24 18:00:58 +0800

    添加文件夹 classes

---
+++ Classes/YLPageScrollView.m
@@ -0,0 +1,49 @@
+//
+//  YLPageScrollView.m
+//  YLPageScrollView
+//
+//  Created by Youngly on 2017/3/13.
+//  Copyright © 2017年 YL. All rights reserved.
+//
+
+#import "YLPageScrollView.h"
+#import "YLPageScrollViewAppreance.h"
+
+@interface YLPageScrollView ()
+
+@property (nonatomic, weak, readwrite) YLPageScrollTitleView *titleView;
+@property (nonatomic, weak, readwrite) YLPageScrollContentView *contentView;
+
+@property (nonatomic, strong) NSArray *childVCs;
+@property (nonatomic, strong) UIViewController *parentVC;
+@property (nonatomic, strong) YLPageScrollViewAppreance *appreance;
+
+@end
+
+
+@implementation YLPageScrollView
+
++ (instancetype)pageScrollView:(CGRect)frame titles:(NSArray *)titles childVCs:(NSArray *)childVCs parentVC:(UIViewController *)parentVC appreance:(YLPageScrollViewAppreance *)appreance
+{
+    return [[self alloc] initWithFrame:frame titles:titles childVCs:childVCs parentVC:parentVC appreance:appreance];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame titles:(NSArray *)titles childVCs:(NSArray *)childVCs parentVC:(UIViewController *)parentVC appreance:(YLPageScrollViewAppreance *)appreance
+{
+    if (self = [super initWithFrame:frame]) {
+        
+        YLPageScrollTitleView *titleView = [YLPageScrollTitleView pageScrollTitleViewWithFrame:CGRectMake(0, 0, frame.size.width, appreance.titleViewHeight) titles:titles appreance:appreance];
+        self.titleView = titleView;
+        [self addSubview:titleView];
+        
+        
+        YLPageScrollContentView *contentView = [YLPageScrollContentView pageScrollContentViewWithFrame:CGRectMake(0, appreance.titleViewHeight, frame.size.width, frame.size.height-appreance.titleViewHeight) childVCs:childVCs parentVC:parentVC appreance:appreance];
+        self.contentView = contentView;
+        [self addSubview:contentView];
+        
+        titleView.delegate_ = contentView;
+        contentView.delegate_ = titleView;
+    }
+    return self;
+}
+@end
